2995f0d86b1fd3ce4b8718c00ea92c778b049d5f
JulianMolina <73005582+Juleeeaaannn@users.noreply.github.com>
2022-04-21 18:56:39 -0300

    Update main.py

--- Actividad 2/main.py
+++ Actividad 2/main.py
@@ -1,8 +1,11 @@
+
 import os
-from viajero import Viajero
+import csv
+from Viajero import Viajero
+from ManejadorViajero import Lista
 if __name__ == '__main__':
     band=1
-    
+    lista=Lista()
     os.system("clear")
     while(band!=-1):
         print('1.leer un archivo,crear instancias de la clase y almacenarlas en una lista')
@@ -11,5 +14,10 @@ if __name__ == '__main__':
         print('4.terminar')
         inp=input('ingrese a donde quiere acceder:')
         if(inp=='1'):
-
+            archivo=open("viajero.csv")
+            reader=csv.reader(archivo,delimiter=';')
+            for fila in reader:
+                objetoV=(fila[0],fila[1],fila[2],fila[3],fila[4])
+                lista.AgregarViajero(objetoV)
             continue
+        
